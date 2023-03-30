12bb8b27e51087d2b20e2f45cca0f61f17e4fb49
APTOL301938 <APTOL301938@APOLBO03-188>
2023-03-30 16:29:02 +0530

    Add project files.

---
+++ scform/Controllers/FormController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace scform.Controllers
+{
+    public class FormController : Controller
+    {
+        // GET: Form
+        public ActionResult Index()
+        {
+            ViewBag.UserTypeList = new List<SelectListItem>
+            {
+             
+            new SelectListItem { Text = "User", Value = "User" },
+            new SelectListItem { Text = "Admin", Value = "Admin" },
+        };
+            return View();
+        }
+    }
+}
---
+++ scform/Models/Users.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace scform.Models
+{
+    public class Users
+
+    {
+       
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserType { get; set; }
+        public string UserDateOfBirth { get; set; }
+        public string UserFile { get; set; }
+        public string AdminName { get; set; }
+        public string AdminEmail { get; set; }
+        public string AdminDateOfBirt { get; set; }
+        public string AdminFile { get; set; }
+      
+    }
+
+    }
